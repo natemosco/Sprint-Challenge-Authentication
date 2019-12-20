6ded4bce3a0f154cde9d8caa1185f22ebfc41ca3
Nathaniel Mosco <natemosco@gmail.com>
2019-12-20 14:09:27 -0500

    added authenticate to a route

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ const authenticate = require("../auth/authenticate-middleware.js");
 const authRouter = require("../auth/auth-router.js");
 const jokesRouter = require("../jokes/jokes-router.js");
 
+const Users = require("../users/users-model");
+
 const server = express();
 
 server.use(helmet());
@@ -17,6 +19,15 @@ server.use("/api/jokes", authenticate, jokesRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "server is working" });
+});
+server.get("api/users", authenticate, (req, res) => {
+  Users.find()
+    .then(res => {
+      res.status(200).json({ message: "server is working", res });
+    })
+    .catch(error => {
+      res.status(500).json({ message: "internal error fetching users" });
+    });
 });
 
 module.exports = server;
