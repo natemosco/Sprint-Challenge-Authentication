36e1f19c7b5b49bbd5aab932b1a00be8b190c206
Nathaniel Mosco <natemosco@gmail.com>
2019-12-20 13:32:32 -0500

    authenticate middleware function written

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -7,5 +7,19 @@ const jwt = require("jsonwebtoken");
 const Users = require("../users/users-model");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: "shall not pass!" });
+  const token = req.headers.authorization;
+
+  if (token) {
+    const secret = process.env.JWT_SECRET || process.env.LOCAL_JWT_SECRET;
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "invalid token" });
+      } else {
+        req.token = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ you: "please login and try again" });
+  }
 };
